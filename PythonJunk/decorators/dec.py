d995729efc679e55390a3dda0cf8a2921cca4b69
jzm0144 <jzm0144@auburn.edu>
2019-09-05 23:27:44 -0500

    futher addition to the decorators part in PythonJunk

--- PythonJunk/decorators/dec.py
+++ PythonJunk/decorators/dec.py
@@ -5,19 +5,28 @@ from time import time
 
 
 def timer(func):
-    def f(x, y=10):
+    def f(*args, **kwargs):
         before = time()
-        rv = func(x, y)
+        rv = func(*args, **kwargs)
         after  = time()
         print('Elasped ',after-before)
         return rv
     return f
 
+def ntimes(n):
+    def inner(f):
+        def wrapper(*args, **kwargs):
+            for _ in range(n):
+                print("running {.__name__}".format(f))
+                rv = f(*args, **kwargs)
+            return rv
+        return wrapper
+    return inner
 
-@timer
+@ntimes(2)
 def add(x, y = 10):
     return x+y
-@timer
+@ntimes(5)
 def sub(x, y=10):
     return x-y
 
