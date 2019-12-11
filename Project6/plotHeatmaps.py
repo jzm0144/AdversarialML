00da42a4f2ffded9bf2a4cddfaeeed1e66ea8b57
jzm0144 <jzm0144@auburn.edu>
2019-12-11 02:23:37 -0600

    ---

--- Project6/plotHeatmaps.py
+++ Project6/plotHeatmaps.py
@@ -4,37 +4,47 @@ import ipdb as ipdb
 import matplotlib.pyplot as plt
 
 
-'''
+
 df = pd.read_csv('allHeatmaps/0.csv')
-df = df.values
-print(df.shape)
+df = df.values[:,1:]
+
 
 plt.figure()
 plt.subplot(211)
-plt.plot(df[:,3])
+plt.plot(df[:,0])
 plt.ylabel("Heat Scores")
+plt.ylim(0, 1.1)
 plt.subplot(212)
 plt.plot(df[:,4:-1])
 plt.xlabel('features')
+plt.ylim(0, 0.015)
 plt.show()
-'''
 
-df = pd.read_csv('avgHeatmaps/1000.csv')
-df = df.values[:,1:]
-print(df.shape)
+
+'''
+df1 = pd.read_csv('avgHeatmaps/1000.csv')
+df1 = df1.values[:,1:]
+df2 = pd.read_csv('avgHeatmaps/1005.csv')
+df2 = df2.values[:,1:]
+df3 = pd.read_csv('avgHeatmaps/1008.csv')
+df3 = df3.values[:,1:]
+
 
 plt.figure()
 plt.subplot(311);
-plt.plot(df[:,0]);
+plt.plot(df1[:,0]);
+plt.ylim(0, 1)
 plt.title('Mean Occlusion Heatmaps')
-plt.ylabel("Heat Scores")
 plt.subplot(312);
-plt.plot(df[:,10]);
+plt.plot(df2[:,5]);
+plt.ylim(0, 1)
+plt.ylabel("Heat Scores")
 plt.subplot(313);
-plt.plot(df[:,21]);
+plt.plot(df3[:,8]);
+plt.ylim(0, 1)
 plt.xlabel('Features')
 plt.show()
-
+'''
 ipdb.set_trace()
 
 
